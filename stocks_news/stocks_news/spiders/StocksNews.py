b7dad3a5ce3c2d1559f119b06d93c6408ee8ec39
Gustavo Mendes <mendes.gustavob@gmail.com>
2021-11-13 16:57:37 -0300

    api investpy && mudança scrapping para news.google

--- stocks_news/stocks_news/spiders/StocksNews.py
+++ stocks_news/stocks_news/spiders/StocksNews.py
@@ -1,14 +1,36 @@
 import scrapy
+import investpy as ipy
 
 
 class StocksnewsSpider(scrapy.Spider):
     name = 'StocksNews'
-    allowed_domains = ['https://www.infomoney.com.br/ultimas-noticias/']
-    start_urls = ['https://www.infomoney.com.br/ultimas-noticias/']
+    #ticker = input('Digite o ticker da ação (exemplo: PETR4): ')
+    ticker = 'PETR4'
+    
+    allowed_domains = ['https://news.google.com/']
+    start_urls = ['https://news.google.com/search?q='+ ticker + '&hl=pt-BR&gl=BR&ceid=BR%3Apt-419']
 
     def parse(self, response):
-        for new in response.css('a::text'):
+        stocks_list = Stocks()
+        stocks_list.set_stock(self.ticker)
+        df = ipy.get_stock_historical_data(stock='PETR4',
+                                        country='brazil',
+                                        from_date='01/01/2010',
+                                        to_date='01/01/2020')
+        print(df.head())
+        for new in response.css('article div div time::text'):
             title = new.get()
-            print(title)
+            stocks_list.date_news(title)
         
         yield {'title': title}
+
+class Stocks:
+    def __init__(self):
+        self.dates_list = []
+        self.stock = ''
+    
+    def date_news(self, date):
+        self.dates_list.append(date)
+        
+    def set_stock(self, stock_name):
+        self.stock = stock_name
